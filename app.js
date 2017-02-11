2656b6c21aed8847780dbc203b3fadaa7a8e058e
john6812711@gmail.com <john6812711@gmail.com>
2017-02-11 15:06:08 +0800

    upadte function

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ var fs = require('fs')
 
 var contents = fs.readFileSync('./package.json', 'utf8')
 //alert(contents)
+
+let myNotification = new Notification('貼心小提醒', {
+  body: '記得帶上鑰匙關上門，天氣冷多加一件外套!'
+})
+
+myNotification.onclick = () => {
+  console.log('Notification clicked')
+}
